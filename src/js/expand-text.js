cd1ab36612c93b0c0bf07ee0e311bcbcfc594417
Felix Khafizov N <asssama212@gmail.com>
2018-05-27 14:54:45 +0500

    Finished unadaptive version of account.html

--- src/js/expand-text.js
+++ src/js/expand-text.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
     $(".more").click(function(event){
         $targ = $(this).prev();
+
+
         if (!$targ.hasClass("expandable-text--expanded")){
+            $targ.css('overflow', 'visible');
+            $targ.css('maxHeight', 'inherit');
+            $height = $targ.css('height');
+            $targ.css('overflow', 'hidden');
+            $targ.css('maxHeight', '40px');
+            $targ.removeClass("expandable-text--collapsed");
             $targ.toggleClass("expandable-text--expanded").animate({
-                maxHeight: "1000px"
-            }, 590);
-            $(this).text('Read More -');
+                maxHeight: $height
+            }, 190);
+            $(this).text('Read Less -');
         } else {
             $targ.toggleClass("expandable-text--expanded").animate({
                 maxHeight: "40px"
