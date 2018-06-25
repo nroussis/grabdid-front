d1175e769f49a0d2cf39377bb34d02ab2e73d8c6
Felix Khafizov N <asssama212@gmail.com>
2018-06-25 20:44:52 +0500

    Changed flag in the header

--- src/js/flag.js
+++ src/js/flag.js
@@ -1,15 +1,44 @@
+$(document).ready(function(){
+    var flagCur = localStorage.getItem('currentFlag');
+
+    if (flagCur !== 'cy' && flagCur !== 'gr') {
+        flagCur = 'gr';
+    }
+
+    var hiddenLangSelector = '.flag-drop__item--' + flagCur;
+
+    $(".flag-drop__item").each(function(){
+        $(this).show();
+    })
+    $(hiddenLangSelector).hide();
+
+    var lang = flagCur.toUpperCase();
+
+    $('.flag-drop__lang').text(lang);
+
+    flagCur = 'flag-drop__selected--' + flagCur;
+    $(".flag-drop__selected").addClass(flagCur);
+});
 //Change lang buttons
-$(".flag-dropdown__item--gr").on("click",function(e){
-    $(".flag-dropdown").removeClass("flag-dropdown__current--cy");
-    $(".flag-dropdown").addClass("flag-dropdown__current--gr");
+
+$(".flag-drop__selected").on("click", function(){
+    $(".flag-drop__list").toggle();
+})
+
+$(".flag-drop__item--gr").on("click",function(e){
+    e.preventDefault();
+    localStorage.setItem('currentFlag', 'gr');
+    location.reload(true);
 });
 
-$(".flag-dropdown__item--cy").on("click",function(e){
-    $(".flag-dropdown").removeClass("flag-dropdown__current--gr");
-    $(".flag-dropdown").addClass("flag-dropdown__current--cy");
+$(".flag-drop__item--cy").on("click",function(e){
+    e.preventDefault();
+    localStorage.setItem('currentFlag', 'cy');
+    location.reload(true);
 });
 
 //Show/hide login buttons
 $("#signButton").on("click", function(){
     $("#loginBlock").slideToggle(200);
 })
+
