daad26c9865c8cc5dea004fdb9058cf679d11c5a
Felix Khafizov N <asssama212@gmail.com>
2018-06-26 02:08:52 +0500

    Some testing and minor edits

--- src/js/flag.js
+++ src/js/flag.js
@@ -1,5 +1,49 @@
+function getCookie(name) {
+
+    var matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ))
+    return matches ? decodeURIComponent(matches[1]) : undefined
+}
+
+// уcтанавливает cookie
+function setCookie(name, value, props) {
+
+    props = props || {}
+
+    var exp = props.expires
+
+    if (typeof exp == "number" && exp) {
+
+        var d = new Date()
+
+        d.setTime(d.getTime() + exp*1000)
+
+        exp = props.expires = d
+
+    }
+
+    if(exp && exp.toUTCString) { props.expires = exp.toUTCString() }
+
+    value = encodeURIComponent(value)
+
+    var updatedCookie = name + "=" + value
+
+    for(var propName in props){
+
+        updatedCookie += "; " + propName
+
+        var propValue = props[propName]
+
+        if(propValue !== true){ updatedCookie += "=" + propValue }
+    }
+
+    document.cookie = updatedCookie
+
+}
+
 $(document).ready(function(){
-    var flagCur = localStorage.getItem('currentFlag');
+    var flagCur = getCookie('currentFlag');
 
     if (flagCur !== 'cy' && flagCur !== 'gr') {
         flagCur = 'gr';
@@ -27,13 +71,13 @@ $(".flag-drop__selected").on("click", function(){
 
 $(".flag-drop__item--gr").on("click",function(e){
     e.preventDefault();
-    localStorage.setItem('currentFlag', 'gr');
+    setCookie('currentFlag', 'gr');
     location.reload(true);
 });
 
 $(".flag-drop__item--cy").on("click",function(e){
     e.preventDefault();
-    localStorage.setItem('currentFlag', 'cy');
+    setCookie('currentFlag', 'cy');
     location.reload(true);
 });
 
